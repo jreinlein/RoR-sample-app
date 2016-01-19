1edccccc406ebd891acb7471fa84a8487bd7c536
jreinlein <jamesreinlein@gmail.com>
2016-01-19 12:59:29 -0500

    Improved validation of emails

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,30 @@ class UserTest < ActiveSupport::TestCase
 		@user.email = "a" * 244 + "@example.com"
 		assert_not @user.valid?
 	end
+
+	test "email validation should accept valid emails" do
+		valid_emails = %w[
+			USER@foo.COM THE_US-ER@foo.bar.org first.last@foo.jp]
+		valid_emails.each do |valid|
+			@user.email = valid
+			assert @user.valid?, "#{valid.inspect} should be valid"
+		end
+	end
+
+	test "email validation should reject invalid emails" do
+		invalid_emails = %w[user@example,com user_at_foo.org 
+			user.name@example. foo@bar_baz.com foo@bar+baz.com]
+		invalid_emails.each do |inv|
+			@user.email = inv
+			assert_not @user.valid?, "#{inv.inspect} should be invalid"
+		end
+	end
+
+	test "emails should be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end
+
 end
